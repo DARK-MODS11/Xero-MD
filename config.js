fdba992b5f020a66e77aec0bb6ddcf96be3d0dd8
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-03-31 20:46:32 +0530

    .

--- config.js
+++ config.js
@@ -4,19 +4,20 @@ const toBool = (x) => x === "true";
 
 module.exports = {
   LOGS: toBool(process.env.LOGS) || true, //false for if you don't want show the logs
-  HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^,',
+  HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^',
   BRANCH: "master",
-  OWNER_NUMBER: ["919961857267@s.whatsapp.net", "918157993101@s.whatsapp.net", "918891871875@s.whatsapp.net", "918943027806@s.whatsapp.net"],
-  STICKER_DATA: process.env.STICKER_DATA || "FINU-USER-BOT;Afx-Nazim",
+  OWNER_NUMBER: ["919961857267@s.whatsapp.net", "918891871875@s.whatsapp.net"],
+  STICKER_DATA: process.env.STICKER_DATA || "FINU-BOT;Afx-Nazim",
   SUDO: process.env.SUDO || "919961857267,918891871875",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "Afx-Nasim",
-  BOT_NAME: process.env.BOT_NAME || "FINU-USER-BOT",
+  MENU_URL: process.env.MENU_URL || "https://www.instagram.com/nazim.x10",
+  IMAGE_URL: process.env.IMAGE_URL ||"https://i.imgur.com/RLTpQS8.jpeg",
+  BOT_NAME: process.env.BOT_NAME || "FINU-BOT",
   THUMB_NAIL: process.env.THUMB_NAIL || "https://i.imgur.com/Uga7UB5.jpeg",
-  ABHI_API: process.env.ABHI_API || "https://abhi-api-bvws.onrender.com/", //Dont change this some commands will not work
-  ALIVE_MSG: process.env.ALIVE_MSG || "Hey {sender}, I'm alive \n Uptime: {uptime}",
-  ALIVE_LOGO: process.env. ALIVE_LOGO || "https://i.imgur.com/KY03JjV.jpeg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "*I AM ALIVE NOW BUDDY*",
+  ALIVE_LOGO: process.env. ALIVE_LOGO || "https://i.imgur.com/jwG0K4A.jpeg",
   WORK_TYPE: process.env.WORK_TYPE || "public",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
